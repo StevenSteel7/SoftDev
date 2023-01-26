f1408ea89839dd267792542b3d5accc51889b4da
Steven <soggsteven92@gmail.com>
2023-01-26 14:34:05 +0530

    Main Page Ready

---
+++ FullStack/Ecommerce/Client/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ReactNode } from 'react';
+
+const Layout = ({children }: {children: React.ReactNode}) => {
+  return (
+    <div>
+        <header>header</header>
+          <main>{children}</main>
+        <footer>footer</footer>  
+    </div>
+    
+  )
+}
+
+export default Layout
